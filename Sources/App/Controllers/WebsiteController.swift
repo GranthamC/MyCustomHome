179f86262f0eee7c34efddae0b017523fe8fcf9c
Craig Grantham <craig.grantham@claytonhomes.com>
2019-04-18 13:28:30 -0400

    Updated web site test code.

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -14,7 +14,14 @@ struct WebsiteController: RouteCollection
 
 	func indexHandler(_ req: Request) throws -> Future<View>
 	{
+		let context = IndexContext(title: "Home Page")
 
-		return try req.view().render("index")
+		return try req.view().render("index", context)
 	}
+}
+
+struct IndexContext: Encodable {
+
+	let title: String
+	
 }
